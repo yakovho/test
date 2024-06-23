abe5205fbedc1a3c06afcadd62a12c2ac4742f24
unknown <yakovhorwitz1@gmail.com>
2024-06-24 00:15:19 +0300

    add class

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("develop")
+
+class demo {
+    constructor(name) {
+        this.name = name
+    }
+    avg() {
+        return (this.name * 2)
+    }
+}
+let one = new demo(5);
+
+console.log(one.avg());
